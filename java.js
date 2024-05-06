0d39e57120c0b2b86e51a23e5e792cc5a76faf22
rafaelbumbumguloso <133383570+rafaelbumbumguloso@users.noreply.github.com>
2024-05-06 10:49:48 -0300

    luiz 3C

---
+++ java.js
@@ -0,0 +1,13 @@
+const textos = document.querySelectorAll(".aba-conteudo");
+textos[i].classlist.add("ativo");
+textos[j].classlist.romove("ativo");
+const contadores = document.querySelectorAll(".contador");
+const comtadores = document.querySelectorAll("contador");
+contadores[0]
+contadores[0].textContent = "contagem regresiva";
+const contaores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new date("2024-10-05");
+const contadores = document.querySelectorAll(".contador");
+consttempoObjetivo1 = new date("2024-10-05");
+contadores[0].textContent = tempoObjetivo1;
+const contadores = document.query
